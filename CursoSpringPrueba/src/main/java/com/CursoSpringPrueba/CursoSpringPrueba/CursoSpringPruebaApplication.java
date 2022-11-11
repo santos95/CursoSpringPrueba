144d4066a59b89f8d08bc85d7692024cfdbc4a80
santos95 <santos95ortiz@gmail.com>
2022-11-11 17:05:09 -0600

    commit

---
+++ CursoSpringPrueba/src/main/java/com/CursoSpringPrueba/CursoSpringPrueba/CursoSpringPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.CursoSpringPrueba.CursoSpringPrueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringPruebaApplication.class, args);
+	}
+
+}
---
+++ CursoSpringPrueba/src/test/java/com/CursoSpringPrueba/CursoSpringPrueba/CursoSpringPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CursoSpringPrueba.CursoSpringPrueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
